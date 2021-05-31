a2b120975788c56213c627b5f7372b70c9c550d0
remiBarlet <rbarlet@protonmail.com>
2021-05-31 23:05:15 +0200

    projets requetes custom

--- api/project/controllers/projectCustom.js
+++ api/project/controllers/projectCustom.js
@@ -23,5 +23,44 @@ module.exports = {
         })
         projectsSlugs.sort(function(a, b){return a['Order']-b['Order']})
         ctx.send(projectsSlugs)
+    },
+
+    async findAllCustom(ctx) {
+        //request to receive all the data for each project
+        let entities = await strapi.services.project.find()
+        //filtering
+        let projects = []
+        entities.forEach(elt => {
+            //filtering the keys slug, id, order, Project_Title and Project_Subtitle
+            let project = {}
+            for (const key in elt) {
+                if ( key == 'Slug'
+                    || key == 'Project_Title'
+                    || key == 'Project_Subtitle'
+                    || key == 'Order'
+                    || key == 'id'
+                    || key == 'Meta_Title'
+                    || key == 'Meta_Description'
+                    || key == 'Pictures') {
+                        project[`${key}`] = elt[key]
+                    }
+            //filtering the elt['Info'] with only the fields not null
+            //in an array 
+            let Info = {}
+
+            for (const key in elt['Info']) {
+                if (elt['Info'][key]) {
+                    Info[`${key}`] = elt['Info'][key]
+                }
+            }
+            //Info array is pushed into project
+            project['Info'] = Info
+            }
+            //project object is pushed in the projects array
+            projects.push(project)
+            //ordering by project['order']
+            projects.sort(function(a, b){return a['Order']-b['Order']})
+            ctx.send(projects)
+        })
     }
 };
